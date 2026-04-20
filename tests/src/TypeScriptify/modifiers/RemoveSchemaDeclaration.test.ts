4f49a23c47b6a393a0a6eaf1554badb5d5b538ed
SignpostMarv <me@signpostmarv.name>
2026-04-20 19:34:28 +0000

    increase branch coverage

--- tests/src/TypeScriptify/modifiers/RemoveSchemaDeclaration.test.ts
+++ tests/src/TypeScriptify/modifiers/RemoveSchemaDeclaration.test.ts
@@ -7,6 +7,8 @@ import {
 
 import {
 	factory,
+	isIdentifier,
+	isVariableStatement,
 } from 'typescript';
 
 // oxlint-disable-next-line @stylistic/max-len
@@ -28,5 +30,52 @@ void describe(`${RemoveSchemaDeclaration.name}::visit()`, () => {
 			instance.visit(declaration),
 			false,
 		);
+	});
+
+	void it('returns undefined when expected', () => {
+		const instance = new RemoveSchemaDeclaration();
+
+		const declaration = factory.createVariableStatement(
+			undefined,
+			[
+				factory.createVariableDeclaration('schema1'),
+			],
+		);
+
+		assert.ok(instance.passes(declaration, {remove_schema: true}));
+		assert.equal(
+			instance.visit(declaration),
+			undefined,
+		);
+	});
+
+	void it('returns statement when expected', () => {
+		const instance = new RemoveSchemaDeclaration();
+
+		const declaration = factory.createVariableStatement(
+			undefined,
+			[
+				factory.createVariableDeclaration('schema1'),
+				factory.createVariableDeclaration('foo'),
+			],
+		);
+
+		assert.ok(instance.passes(declaration, {remove_schema: true}));
+
+		assert.equal(declaration.declarationList.declarations.length, 2);
+		assert.ok(isIdentifier(
+			declaration.declarationList.declarations[1].name,
+		));
+		assert.equal(
+			declaration.declarationList.declarations[1].name.text,
+			'foo',
+		);
+
+		const result = instance.visit(declaration);
+
+		assert.ok(result && isVariableStatement(result));
+		assert.equal(result.declarationList.declarations.length, 1);
+		assert.ok(isIdentifier(result.declarationList.declarations[0].name));
+		assert.equal(result.declarationList.declarations[0].name.text, 'foo');
 	});
 });
