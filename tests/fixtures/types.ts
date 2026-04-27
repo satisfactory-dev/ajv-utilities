ada0e8b78fe1fd1cd698d3785bfac0ba73d62e16
SignpostMarv <me@signpostmarv.name>
2026-04-27 18:15:35 +0000

    adjusting indentation

--- tests/fixtures/types.ts
+++ tests/fixtures/types.ts
@@ -1,8 +1,8 @@
 type const_string = {type: 'string', const: string};
 
 type enum_string<
-    T0 extends string = string,
-    T1 extends string = string,
+	T0 extends string = string,
+	T1 extends string = string,
 > = {
 	type: 'string',
 	enum: [T0, T1, ...string[]],
